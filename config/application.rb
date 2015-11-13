dfc5c723f38019f9fb8ea82d752945027044dadd
yonda <honda@33i.co.jp>
2015-11-13 14:11:02 +0900

    add Allow-Credentials and Origin

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,11 @@ module Urasan
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Allow-Origin' => 'chrome-extension://dbkjmahdlenondbjpbedefbcmlkbicai',
+      'Access-Control-Request-Method' => '*'
+    }
   end
 end
