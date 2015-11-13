ec5c75feea142d5e74df148aafb6ad3b0ac20899
yonda <honda@33i.co.jp>
2015-11-13 13:23:48 +0900

    change dislike create path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :threads, only: [] do
     resources :messages, only: [] do
-      resources :dislikes, only: %w(create index)
+      resources :dislikes, only: %w(index)
+      post 'dislikes/create' => 'dislikes#create'
       post 'dislikes/destroy' => 'dislikes#destroy'
     end
   end
